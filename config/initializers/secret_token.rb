bfec69dd793e125493c10b228cae93d6b6adfe33
richfisher <richfisher.pan@gmail.com>
2013-06-05 18:13:27 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarionetteDemo::Application.config.secret_token = 'c9232b8aeb24ea414cc62b8260bdd427afc0aefcd71daf209db21c681125bf83933a3b0f19f1e048f2e8bea9b990f6cc1642c516cb6e1e3eab0ddccb7205b647'
